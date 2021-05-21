6b86206b336bd021cfdc58b8c37d11f74ad85ce0
NagataKouya <nagata.twi@gmail.com>
2021-05-21 10:39:53 +0900

    [add] lottie

---
+++ src/components/Bubble/index.tsx
@@ -0,0 +1,20 @@
+import styles from './index.module.css';
+
+const Bubble = () => (
+  <section className={styles.sticky}>
+    <div className={styles.bubbles}>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+    </div>
+  </section>
+);
+
+export default Bubble;
