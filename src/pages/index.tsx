36d6d2ddb30c755408512e9ddc8172fb0e5f66c9
ikmz <ikmz0104.official@gmail.com>
2021-08-11 21:47:00 +0900

    feat: ログアウト処理が走らないため元に戻します

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,6 +59,7 @@ const Home: React.FC = (props) => {
   return (
     <>
       <div>
+        <pre>{currentUser && JSON.stringify(currentUser, null, 4)}</pre>
         <button onClick={logOut}>Logout</button>
       </div>
       <Header title="みーたんタイマー" />
