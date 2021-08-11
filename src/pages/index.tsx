802c18607c18551c4fb7e297d7e97312d4716c46
ikmz <ikmz0104.official@gmail.com>
2021-08-11 15:54:45 +0900

    WIP: E-mail+Password認証
    onAuthStateChangedが参照できないエラー

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,35 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import dynamic from 'next/dynamic';
-import firebase from 'util/firebase';
-import CategoryList from 'components/CategoryList';
-import Header from 'components/Header';
-import Calendar from 'components/Calendar';
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import dynamic from 'next/dynamic'
+import firebase from 'util/firebase'
+import CategoryList from 'components/CategoryList'
+import Header from 'components/Header'
+import Calendar from 'components/Calendar'
+import { auth } from '../../lib/db'
+import { useRouter } from 'next/router'
 
 //ApexCharts読み込むのにNext.jsで必要な設定
 const DynamicGraphComponentWithNoSSR = dynamic(() => import('../views/Graph/Graph'), { ssr: false });
 
 const Home: React.FC = (props) => {
   const [categories, setCategories] = useState([]);
+  const router = useRouter()
+  const [currentUser, setCurrentUser] = useState<null | object>(null)
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      user ? setCurrentUser(user) : router.push('/login')
+    })
+  }, [])
+
+  const logOut = async () => {
+    try {
+      await auth.signOut()
+      router.push('/login')
+    } catch (error) {
+      alert(error.message)
+    }
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -39,6 +58,10 @@ const Home: React.FC = (props) => {
 
   return (
     <>
+      <div>
+        <pre>{currentUser && JSON.stringify(currentUser, null, 4)}</pre>
+        <button onClick={logOut}>Logout</button>
+      </div>
       <Header title="みーたんタイマー" />
       <div className="content">
         <div style={{ marginBottom: 40 }}>
