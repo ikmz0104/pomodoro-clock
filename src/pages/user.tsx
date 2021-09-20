f471586ea89d9f8fb638d51fd746ea2a0739c0b1
ikmz <ikmz0104.official@gmail.com>
2021-09-20 22:05:09 +0900

    Complex buttonsを追加

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Header from 'components/Header';
 import SimpleBottomNavigation from 'views/Navigation';
 import Box from '@mui/material/Box';
@@ -8,51 +8,159 @@ import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import ButtonBase from '@mui/material/ButtonBase';
+
+const images = [
+  {
+    url: '/static/images/buttons/breakfast.jpg',
+    title: 'Breakfast',
+    width: '40%',
+  },
+  {
+    url: '/static/images/buttons/burgers.jpg',
+    title: 'Burgers',
+    width: '30%',
+  },
+  {
+    url: '/static/images/buttons/camera.jpg',
+    title: 'Camera',
+    width: '30%',
+  },
+];
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important', // Overrides inline-style
+    height: 100,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+}));
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}));
 
 const user = () => {
   return (
     <div className="container">
       <Header title="ユーザー" />
       <div className="content">
-      <Box sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper' }}>
-      <Box sx={{ my: 3, mx: 2 }}>
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h4" component="div">
-              Toothbrush
+        <Box sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper' }}>
+          <Box sx={{ my: 3, mx: 2 }}>
+            <Grid container alignItems="center">
+              <Grid item xs>
+                <Typography gutterBottom variant="h4" component="div">
+                  Username
+                </Typography>
+              </Grid>
+            </Grid>
+            <Typography color="text.secondary" variant="body2">
+              Pinstriped cornflower blue cotton blouse takes you on a walk to the park or just down the hall.
             </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h6" component="div">
-              $4.50
+          </Box>
+          <Divider variant="middle" />
+          <Box sx={{ m: 2 }}>
+            <Typography gutterBottom variant="body1">
+              Select type
             </Typography>
-          </Grid>
-        </Grid>
-        <Typography color="text.secondary" variant="body2">
-          Pinstriped cornflower blue cotton blouse takes you on a walk to the park or
-          just down the hall.
-        </Typography>
-      </Box>
-      <Divider variant="middle" />
-      <Box sx={{ m: 2 }}>
-        <Typography gutterBottom variant="body1">
-          Select type
-        </Typography>
-        <Stack direction="row" spacing={1}>
-          <Chip label="Extra Soft" />
-          <Chip color="primary" label="Soft" />
-          <Chip label="Medium" />
-          <Chip label="Hard" />
-        </Stack>
-      </Box>
-      <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
-        <Button>Add to cart</Button>
-      </Box>
-    </Box>
+            <Stack direction="row" spacing={1}>
+              <Chip label="Extra Soft" />
+              <Chip label="Soft" />
+              <Chip label="Medium" />
+              <Chip label="Hard" />
+            </Stack>
+          </Box>
+          <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
+            <Button>Logout</Button>
+          </Box>
+        </Box>
+        <hr />
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+          {images.map((image) => (
+            <ImageButton
+              focusRipple
+              key={image.title}
+              style={{
+                width: image.width,
+              }}
+            >
+              <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+              <ImageBackdrop className="MuiImageBackdrop-root" />
+              <Image>
+                <Typography
+                  component="span"
+                  variant="subtitle1"
+                  color="inherit"
+                  sx={{
+                    position: 'relative',
+                    p: 4,
+                    pt: 2,
+                    pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                  }}
+                >
+                  {image.title}
+                  <ImageMarked className="MuiImageMarked-root" />
+                </Typography>
+              </Image>
+            </ImageButton>
+          ))}
+        </Box>
       </div>
       <SimpleBottomNavigation />
     </div>
-  )
-}
+  );
+};
 
-export default user
+export default user;
